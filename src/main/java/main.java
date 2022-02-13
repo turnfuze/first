a22b9aff6834584c435e704aaaa8ed5694161a80
Yaroslav <muontarn@gmail.com>
2022-02-13 10:01:25 +0200

    Main task done

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,15 +2,11 @@
 public class main {
 
     public static void main(String[] args) {
-        //variable.variable();
-        //conditions.length();
-        //conditions.vasya();
-        //conditions.conversely();
-        //conditions.sum();
-        //conditions.weight();
-        //conditions.moreOrSmaller();
-        //conditions.decreaseBy1();
-        //conditions.derivative();
-        //conditions.parityDegree();
+//        cycle.cycle4();
+//        cycle.factorial();
+//        array.listShow();
+//        array.addNumbers();
+        array.twoArraes();
+
     }
 }
