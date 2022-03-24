46ba6ba86e3333a20ae78c23e35c508b8db3bb0f
Yaroslav <muontarn@gmail.com>
2022-03-24 16:48:39 +0200

    Add minerals object and override toString and equals

--- src/main/java/OOP/Minerals.java
+++ src/main/java/OOP/Minerals.java
@@ -1,29 +1,44 @@
 package OOP;
 
-public class Minerals {
-        String name;
-        String color;
-        int density;
+import java.util.Objects;
 
-    public Minerals(String name, String color, int density){
+public class Minerals {
+    String name;
+    String color;
+    int density;
+
+    public Minerals(String name, String color, int density) {
         this.name = name;
         this.color = color;
         this.density = density;
     }
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
 
-    public String getColor(){
+    public String getColor() {
         System.out.println(this.color);
-        return  this.color;
+        return this.color;
     }
 
-    public  int getDensity(){
+    public int getDensity() {
         System.out.println(this.density);
         return this.density;
     }
 
+    @Override
+    public String toString() {
+
+        return String.format("Name %s, Color %s, Density %n", name, color, density);
+    }
+
+        @Override
+    public boolean equals(Object o){
+        if(this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Minerals mineral = (Minerals) o;
+        return Objects.equals(name, mineral.name) && Objects.equals(color, mineral.color) && Objects.equals(density, mineral.density);
+    }
 
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,10 @@
-
+import OOP.Minerals;
 
 public class main {
 
     public static void main(String[] args) {
-
+        Minerals mineral = new Minerals("Gold","Yellow", 15);
+        System.out.println(mineral);
 
 
     }
