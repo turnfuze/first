df36e66359100e041c894ace3ec0fda58042f6b4
Yaroslav <muontarn@gmail.com>
2022-04-07 18:02:19 +0300

    I'm lerned use interface and abstract class

---
+++ src/main/java/OOP/Rocks/Gematit.java
@@ -0,0 +1,27 @@
+package OOP.Rocks;
+
+import OOP.Stoneable;
+
+public final class Gematit extends Rock implements Stoneable {
+    public float amount;
+
+    public Gematit(String name, String color, String includedMetal, float amount) {
+        super(name, color, includedMetal);
+        this.amount = amount;
+    }
+
+    public void slice() {
+        System.out.println(" Slice on squares");
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Name: %s, Color: %s, SecondColor: %s, Texture: %s, Surface: %s", name, color);
+    }
+
+    @Override
+    public void melting(Rock rock) {
+        float weight = amount * indexLossesOre;
+        System.out.println("You get bulling of " + this.includedMetal + " weight: " + weight + " g");
+    }
+}
