b8634518159930157902f45c8aa6c604b8c1ba19
Yaroslav <muontarn@gmail.com>
2022-05-09 15:02:48 +0300

    Удалена статика, все методы возвращают листыÐ

--- src/main/java/marketplace/ProductMarket.java
+++ src/main/java/marketplace/ProductMarket.java
@@ -10,59 +10,34 @@ public class ProductMarket {
         this.products = products;
     }
 
-//    public List<Products> sortUp(List<Products> array) {
-//        List<Products> sortedList = array.stream().filter(products1 -> products1.getPrice() > 10);
-//        sortedList = products.stream().map(products -> products.getName());
-//        return sortedList;
-//    }
-
-    public  static List<String> getAllNames(List<Products> products){
-        List<String> namesOfProducts = products.stream().
-                map(product -> product.getName()).
-                sorted().
-                collect(Collectors.toList());
-        return namesOfProducts;
+    public List<String> getAllNames() {
+        return products.stream()
+                .map(product -> product.getName())
+                .sorted()
+                .collect(Collectors.toList());
     }
 
-    public  static void getAllNamesSOUT(List<Products> products){
-              products.stream().forEach(prod1->System.out.print(prod1.getName()));
+    public List<Products> more10() {
+        return products.stream()
+                .filter(product -> product.getPrice() > 10)
+                .collect(Collectors.toList());
     }
 
-    public static void more10SOUT (List<Products> products){
-        products.stream().
-                filter(product -> product.getPrice()>10).
-                forEach(prod -> System.out.println(prod.getPrice()));
+    public List<Products> smaller0() {
+        return products.stream()
+                .filter(prod -> prod.getPrice() < 0)
+                .collect(Collectors.toList());
     }
 
-    public static List<Products> more10 (List<Products> products){
-        List<Products> productsMore10 = products.stream().
-                filter(product -> product.getPrice()>10).
-                collect(Collectors.toList());
-        return productsMore10;
+    public List<?> returnPriceUsString() {
+        return products.stream()
+                .map(product -> product.getPrice())
+                .collect(Collectors.toList());
     }
 
-    public static List<Products> smaller0(List<Products> products){
-        List<Products> pricesSmaller0 = products.stream().
-                filter(prod->prod.getPrice()<0).
-                collect(Collectors.toList());
-        return pricesSmaller0;
+    public List<?> returnPriceUsOneString() {
+        return products.stream()
+                .map(product -> product.getPrice())
+                .collect(Collectors.toList());
     }
-
-    public static void smaller0SOUT(List<Products> products){
-         products.stream().
-                 filter(prod->prod.getPrice()<0).
-                 forEach(prod-> System.out.println(prod.getPrice()));
-
-    }
-
-    public static List returnPriceUsString(List<Products> products){
-        List price= products.stream().map(product -> product.getPrice()).collect(Collectors.toList());
-        System.out.println("test " + 4 + price.get(1));
-         return price;
-    }
-
-    public static List returnPriceUsOneString(List<Products> products){
-        return products.stream().map(product -> product.getPrice()).collect(Collectors.toList());
-    }
-
 }
--- src/main/java/marketplace/main.java
+++ src/main/java/marketplace/main.java
@@ -1,27 +1,29 @@
 package marketplace;
+
 import java.util.List;
 
-import static marketplace.ProductMarket.*;
+import marketplace.ProductMarket;
+import marketplace.ProductMarket.*;
+
 public class main {
     public static void main(String[] args) {
-        List<Products> eat ;
-       Products water = new Products("Bottle of water", 12);
-    Products bread = new Products("Bread", 3);
-    Products milk = new Products("Milk", 2);
-    Products juice = new Products("Juice", 4);
-    Products wheat = new Products("Wheat", 5);
-    Products cheese = new Products("Cheese", 7);
-    Products apple = new Products("Apple", 1);
-    Products butter = new Products("Butter", 15);
-        List<Products> eat1 = List.of(water,bread,milk,juice,wheat,cheese,butter,apple);
+        Products water = new Products("Bottle of water", 12);
+        Products bread = new Products("Bread", 3);
+        Products milk = new Products("Milk", 2);
+        Products juice = new Products("Juice", 4);
+        Products wheat = new Products("Wheat", 5);
+        Products cheese = new Products("Cheese", 7);
+        Products apple = new Products("Apple", 1);
+        Products butter = new Products("Butter", 15);
+        List<Products> eat1 = List.of(water, bread, milk, juice, wheat, cheese, butter, apple);
 
+        ProductMarket products = new ProductMarket(eat1);
 
-        getAllNamesSOUT(eat1);//список всех продуктов
-        more10SOUT(eat1);  //отдельно вывожу цены
-        System.out.println(more10(eat1));    // отдельно вывожу отфильтрованые объекты по цене больше 10
-        System.out.println(getAllNames(eat1));   // вывожу имена всех продуктов которые отсортированы а-z
-        smaller0SOUT(eat1); // меньше ноля
-        System.out.println(returnPriceUsString(eat1)); // цены в списке как стринг
-
+        System.out.println(products.getAllNames());         //список всех продуктов
+        System.out.println(products.more10());              //отдельно вывожу цены
+        System.out.println(products.more10());              // отдельно вывожу отфильтрованые объекты по цене больше 10
+        System.out.println(products.getAllNames());         // вывожу имена всех продуктов которые отсортированы а-z
+        System.out.println(products.smaller0());            // меньше ноля
+        System.out.println(products.returnPriceUsString()); // цены в списке как стринг
     }
 }
